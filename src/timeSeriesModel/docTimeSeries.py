b7b8f966d689cee39d9494fea96d3b90fda6fd2f
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-03-27 13:56:47 +0530

    Started with a time series model. HMM probably

--- src/timeSeriesModel/docTimeSeries.py
+++ src/timeSeriesModel/docTimeSeries.py
@@ -2,27 +2,23 @@ from WordToVec import *
 from sklearn.cluster import KMeans
 import math
 import numpy as np
-class docVector:
-    def __init__(self,k):
-        self.wv = wordVecGen() # Generator for Word Vectors
-        self.Model = KMeans(k)
-        self.Model.fit(self.wv.word_vectors.values())
-        self.gen(k)
-        self.topicSeries = {}
-    def gen(self,k):
-        #This part for English
-        for topic in self.wv.topics_en:
-            topic_series = []
-            f=open('files/'+topic+'.txt','r')
-            content=f.read()
+class docTimeSeries:
+    def __init__(self, wordVecGen, k):
+        self.wordVecGen = wordVecGen
+        self.Model      = KMeans(k)
+        self.Model.fit(self.wordVecGen.word_vectors.values())
+        self.genSeries()
+
+    def genSeries(self):
+
+        #This part for english
+        print "Working on English"
+        stop_en = stopwords.words('english')
+        stop_en = [unicode(i) for i in stop_en ]
+        st_en = LancasterStemmer()
+        for topic in self.wordVecGen.topics_en:
+            f = open('files/'+topic+'.txt','r')
+            content = f.read()
             f.close()
             print "Loaded",topic
-            sentences = content.split('. ')
-            self.topicSeries[topic]=np.array(topic_series)
-
-        #This part for french
-        for topic in self.wv.topics_fr:
-            topic_series = []
-            for word in self.wv.corpus_fr[topic]:
-                topic_series.append(self.Model.predict([self.wv.word_vectors[word]])[0])
-            self.topicSeries[topic]=np.array(topic_series)
+            
