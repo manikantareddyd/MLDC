3f9c158a641208729fa79ac91033a48db10b74c7
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-03-29 19:17:57 +0530

    Better and universal way to extract wikipedia articles. Needs revision of code to fix for new wikipedia extractor

--- src/getDocFromWiki.py
+++ src/getDocFromWiki.py
@@ -1,9 +1,32 @@
+import urllib2
+from bs4 import BeautifulSoup
 import wikipedia
 import sys
 
-wikipedia.set_lang(str(sys.argv[1]))
-page    = wikipedia.page(str(sys.argv[2]))
+print "Topic ",str(sys.argv[1])
+soup = BeautifulSoup(urllib2.urlopen('http://en.wikipedia.org/wiki/'+str(sys.argv[1])), "lxml")
+links = [(el.get('lang'), el.get('title')) for el in soup.select('li.interlanguage-link > a')]
+
+# This one is for english
+wikipedia.set_lang('en')
+print "en ",str(sys.argv[1])
+page    = wikipedia.page(str(sys.argv[1]))
 content = page.content.lower()
-f=open('files/'+str(sys.argv[2])+'_'+str(sys.argv[1])+'.txt','w')
-content=f.write(content.encode('utf8'))
+f=open('files/'+str(sys.argv[1])+'_en'+'.txt','w')
+f.write(content.encode('utf8'))
 f.close()
+
+for language, title in links:
+    if language not in ['fr']:
+        continue
+    page_title = title.split(u' - ')[0]
+    try:
+        print language + ' ' + str(page_title.split(' ')[0])
+    except:
+        print language + ' '  + str(sys.argv[1])
+    wikipedia.set_lang(language)
+    page    = wikipedia.page((page_title.split(' ')[0]))
+    content = page.content.lower()
+    f=open('files/'+str(sys.argv[1])+'_'+str(language)+'.txt','w')
+    f.write(content.encode('utf8'))
+    f.close()
