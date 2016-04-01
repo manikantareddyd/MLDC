ce1135a741aece4984aab0902008b19a7586e013
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-04-01 16:14:34 +0530

    Modularised Functions for porting to multiple languages

--- src/freqModel/WordToVec.py
+++ src/freqModel/WordToVec.py
@@ -11,130 +11,95 @@ class wordVecGen:
     def __init__(self):
         # Initializations...
         print "Initializing..."
-        self.corpus_en = {}
-        self.topics = ['Adele','Baboon','Chemistry','Energy','English_language','French_language','India','Pokemon','Pakistan','Politics','Tennis','The_Beatles','Wikipedia']
-        # self.topics = self.topics[:]
-        self.topics_en_test = ['Pokemon_en_test','India_en']
-        self.topics_en = [i+'_en' for i in self.topics]+self.topics_en_test
-        self.corpus_fr = {}
-        self.topics_fr_test = ['Pokemon_fr_test','India_fr']
-        self.topics_fr = [i+'_fr' for i in self.topics]+self.topics_fr_test
+        self.languages = ['en','fr']
+        self.topics_all = ['Adele','Baboon','Chemistry','Pokemon','Energy','English_language','French_language','India','Pakistan','Politics','Tennis','The_Beatles','Wikipedia']
+        self.topics_all = self.topics_all[:4]
+        self.corpus = {}
+        self.topics = {}
+        self.topics_train = {}
         self.word_vectors = {}
-        self.word_vectors_en = {}
-        self.word_vectors_fr = {}
+        self.topics['en_test'] = ['Pokemon_en_test','Chemistry_en']
+        self.topics['fr_test'] = ['Pokemon_fr_test','Chemistry_fr']
+
+        for k in self.languages:
+            self.topics[k] = [i +'_'+ k for i in self.topics_all]+self.topics[k+'_test']
+            self.topics_train[k] = [i +'_'+ k for i in self.topics_all]
+            self.word_vectors[k] = {}
+            self.corpus[k] = {}
+
+        self.word_vectors_all = {}
+
         print "Topics Chosen"
         t = PrettyTable(['English', 'French'])
-        for i in range(len(self.topics_en)):
-            t.add_row([self.topics_en[i],self.topics_fr[i]])
+        for i in range(len(self.topics['en'])):
+            t.add_row([self.topics['en'][i],self.topics['fr'][i]])
         print t
         # Now lets Generate
         self.gen()
 
     def gen(self):
-        # This Part for English
-        print "Working on English"
-        topics_en = self.topics_en
-        words_en  = []
-        stop_en = stopwords.words('english')
-        stop_en = [unicode(i) for i in stop_en ]
-        st_en = LancasterStemmer()
-        freqDist_en={}
-        for topic in topics_en:
-            # Just the Laborious File Loading tasks
-            f=open('files/'+topic+'.txt','r')
-            content=f.read()
-            f.close()
-            print "Loaded",topic
 
-            # This Part now stems every word in the document loaded and copies it into corpus_en.
-            words_in_topic  = [(st_en.stem(unicode(i))) for i in (re.findall("[a-zA-Z]+", content))]
+        words = {}
+        short_lang = {'en':['english',LancasterStemmer] , 'fr':['french',FrenchStemmer]}
+        freqDist = {}
+        topics = {}
+        for k in self.languages:
+            # This Part for English
+            print "Working on" + k
+            topics[k] = self.topics[k]
+            words[k] = []
+            stop = stopwords.words(short_lang[k][0])
+            stop = [unicode(i) for i in stop ]
+            st = short_lang[k][1]
 
-            # Generate a histogram of words in the current document
-            freqDist_en[topic]={x:words_in_topic.count(x) for x in words_in_topic}
-            for word in freqDist_en[topic].keys()[:]:
-                if word in stop_en:
-                    del freqDist_en[topic][word]
+            freqDist[k]={}
 
-            # Applying a term frequency function... A factor to be experimented on...
-            # Current Function: exp(frequency/(1+max(frequencies)))
-            maxval = max(freqDist_en[topic].values())
-            freqDist_en[topic]={x:math.exp(freqDist_en[topic][x]/(1.0*maxval)) for x in freqDist_en[topic].keys()}
+            for topic in topics[k]:
+                # Just the Laborious File Loading tasks
+                f=open('files/'+topic+'.txt','r')
+                content=f.read()
+                f.close()
+                print "Loaded",topic
 
-            # Add all the words in the current document to all words list
-            words_en += freqDist_en[topic].keys()
+                # This Part now stems every word in the document loaded and copies it into corpus['en'].
+                words_in_topic  = [(st().stem(unicode(i))) for i in (re.findall("[a-zA-Z]+", content))]
 
-            # Copy all words in the topic to corpus_en
-            self.corpus_en[topic] = freqDist_en[topic].keys()
+                # Generate a histogram of words in the current document
+                freqDist[k][topic]={x:words_in_topic.count(x) for x in words_in_topic}
+                for word in freqDist[k][topic].keys()[:]:
+                    if word in stop:
+                        del freqDist[k][topic][word]
 
-        # Converting the list of words into a set of words so that duplicates are removed
-        print "Number words in English: "+str(len(words_en))
-        words_en = list(set(words_en))
-        print "Number words in English as set: "+str(len(words_en))
+                # Applying a term frequency function... A factor to be experimented on...
+                # Current Function: exp(frequency/(1+max(frequencies)))
+                maxval = max(freqDist[k][topic].values())
+                freqDist[k][topic]={x:math.exp(freqDist[k][topic][x]/(1.0*maxval)) for x in freqDist[k][topic].keys()}
 
-        print "Generating vectors for english words"
-        for word in words_en:
-            vec=[]
-            for topic in topics_en:
-                try:
-                    vec.append(freqDist_en[topic][unicode(word)])
-                except:
-                    vec.append(0)
-            self.word_vectors[word]=vec
-            self.word_vectors_en[word]=vec
-        print "All Done with English"
-        print "-"*48
+                # Add all the words in the current document to all words list
+                words[k] += freqDist[k][topic].keys()
+
+                # Copy all words in the topic to corpus['en']
+                self.corpus[k][topic] = freqDist[k][topic].keys()
+
+            # Converting the list of words into a set of words so that duplicates are removed
+            print "Number words in English: "+str(len(words[k]))
+            words[k] = list(set(words[k]))
+            print "Number words in English as set: "+str(len(words[k]))
+
+            print "Generating vectors for english words"
+            for word in words[k]:
+                vec=[]
+                for topic in self.topics_train[k]:
+                    try:
+                        vec.append(freqDist[k][topic][unicode(word)])
+                    except:
+                        vec.append(0)
+                self.word_vectors_all[word]=vec
+                self.word_vectors[k][word]=vec
+            print "All Done with English"
+            print "-"*48
 
         #-----------------------------------------------------------------------------------#
 
-        # This Part for French
-        print "Working on French"
-        topics_fr = self.topics_fr
-        words_fr  = []
-        stop_fr = stopwords.words('french')
-        #stop_fr = [unicode(i) for i in stop_fr ]
-        st_fr = FrenchStemmer()
-        freqDist_fr={}
-        for topic in topics_fr:
-            # Just the Laborious File Loading tasks
-            f=open('files/'+topic+'.txt','r')
-            content=f.read()#content.encode('utf8'))
-            f.close()
-            print "Loaded",topic
-            # This Part now stems every word in the document loaded and copies it.
-            words_in_topic = [(st_fr.stem(unicode(i))) for i in (re.findall("[a-zA-Z]+", content))]
 
-            # Generate a histogram of words in the current document
-            freqDist_fr[topic]={x:words_in_topic.count(x) for x in words_in_topic}
-            for word in freqDist_fr[topic].keys()[:]:
-                if word in stop_fr:
-                    del freqDist_fr[topic][word]
-
-            # Applying a term frequency function... A factor to be experimented on...
-            # Current Function: exp(frequency/(1+max(frequencies)))
-            maxval = max(freqDist_fr[topic].values())
-            freqDist_fr[topic]={x:math.exp(freqDist_fr[topic][x]/(1.0*maxval)) for x in freqDist_fr[topic].keys()}
-
-            # Add all the words in the current document to all words list
-            words_fr += freqDist_fr[topic].keys()
-
-            # Copy all words in the topic to corpus_fr
-            self.corpus_fr[topic] = freqDist_fr[topic].keys()
-
-        # Converting the list of words into a set of words so that duplicates are removed
-        print "Number words in French: "+str(len(words_fr))
-        words_fr = list(set(words_fr))
-        print "Number words in French as set: "+str(len(words_fr))
-
-        print "Generating vectors for french words"
-        for word in words_fr:
-            vec=[]
-            for topic in topics_fr:
-                try:
-                    vec.append(freqDist_fr[topic][word])
-                except:
-                    vec.append(0)
-            self.word_vectors[word]=vec
-            self.word_vectors_fr[word]=vec
-        print "All Done with French"
-        print "-"*48
         print "All tasks Done... Exiting Now\n"
--- src/freqModel/doc2vec.py
+++ src/freqModel/doc2vec.py
@@ -7,34 +7,29 @@ class docVector:
     def __init__(self,wordVecGen,k):
         self.wv = wordVecGen # Generator for Word Vectors
         self.Model = KMeans(k)
-        self.Model.fit(self.wv.word_vectors.values())
-        print "Size of Vocabulary: "+str(len(self.wv.word_vectors))
+        self.Model.fit(self.wv.word_vectors_all.values())
+        print "Size of Vocabulary: "+str(len(self.wv.word_vectors_all))
+        self.languages = ['en','fr']
         self.gen(k)
 
     def gen(self,k):
-        topics_en_test = self.wv.topics_en_test
-        topics_fr_test = self.wv.topics_fr_test
+
+        topics_test = {}
         self.topicVectors={}
+        for j in self.languages:
 
-        #This part for English
-        print "Vectorizing English docs"
-        for topic in topics_en_test:
-            vector = [0 for x in range(k)]
-            print "Generating vector for ",topic
-            for word in self.wv.corpus_en[topic]:
-                vector[self.Model.predict([self.wv.word_vectors[word]])[0]]+=1
-            vector = [math.exp((1+vector[x])/(1.0+1.0*max(vector))) for x in range(k)]
-            self.topicVectors[topic]=np.array(vector)
+            topics_test[j] = self.wv.topics[j+'_test']
 
-        #This part for French
-        print "\nVectorizing French docs"
-        for topic in topics_fr_test:
-            vector = [0 for x in range(k)]
-            print "Generating vector for ",topic
-            for word in self.wv.corpus_fr[topic]:
-                vector[self.Model.predict([self.wv.word_vectors[word]])[0]]+=1
-            vector = [math.exp((1+vector[x])/(1.0+1.0*max(vector))) for x in range(k)]
-            self.topicVectors[topic]=np.array(vector)
+            #This part for English
+            print "Vectorizing English docs"
+            for topic in topics_test[j]:
+                vector = [0 for x in range(k)]
+                print "Generating vector for ",topic
+                for word in self.wv.corpus[j][topic]:
+                    vector[self.Model.predict([self.wv.word_vectors_all[word]])[0]]+=1
+                vector = [math.exp((1+vector[x])/(1.0+1.0*max(vector))) for x in range(k)]
+                self.topicVectors[topic]=np.array(vector)
+
 
         print "All docs vectorized"
 
@@ -42,8 +37,8 @@ wordVecGen = wordVecGen()
 d=docVector(wordVecGen,20)
 n = np.linalg.norm
 v=d.topicVectors
-for topic in d.wv.topics_en_test:
-    for t in d.wv.topics_fr_test:
+for topic in d.wv.topics['en_test']:
+    for t in d.wv.topics['fr_test']:
         print t,topic,1,n(v[t]-v[topic],1)
         # print t,topic,2,n(v[t]-v[topic],2)
         # print t,topic,"cosine",cosine(v[t],v[topic])
