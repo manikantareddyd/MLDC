75418f589a981a706e8d1063003fb27b38e31398
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-04-03 10:25:10 +0530

    Improved Multi Threading. Realised that there is no necessity of a thread for every word and it increases time rather than decreasing it.

--- src/freqModel/doc2vec.py
+++ src/freqModel/doc2vec.py
@@ -12,30 +12,51 @@ class docVector:
         self.Model.fit(self.wv.word_vectors_all.values())
         self.languages = self.wv.languages
         self.topicVectors={}
+        self.topicVectorsTrain={}
         threads = {}
         for j in self.languages:
-            threads[j]=threading.Thread( target=self.gen, args=(k,j,))
-            print "Vectorizing "+j+" docs"
-            threads[j].start()
+            threads[j+'Train']=threading.Thread(target=self.genTrain, args=(k,j,))
+            threads[j+'Train'].start()
+        for j in self.languages:
+            threads[j+'Train'].join()
+        print "\nTraining Set Done\n"
+        for j in self.languages:
+            threads[j+'Test']=threading.Thread( target=self.genTest, args=(k,j,))
+            threads[j+'Test'].start()
             # self.gen(k,j)
         for j in self.languages:
-            threads[j].join()
+            threads[j+'Test'].join()
         print "All docs vectorized"
 
-    def gen(self,k,j):
-        #This part for English
+    def genTrain(self,k,j):
+        threadTopic = {}
+        for topic in self.wv.topics[j]:
+            threadTopic[topic] = threading.Thread(target = self.vectorGenTrain, args = (topic,k,j))
+            threadTopic[topic].start()
 
+        for topic in self.wv.topics[j]:
+            threadTopic[topic].join()
+            print "Loaded", topic
+
+    def vectorGenTrain(self,topic,k,j):
+        vector = [0 for x in range(k)]
+        for word in self.wv.corpus[j][topic]:
+            vector[self.Model.predict([self.wv.word_vectors[j][word]])[0]]+=1
+        vector = [math.exp((1+vector[x])/(1.0+1.0*max(vector))) for x in range(k)]
+        self.topicVectorsTrain[topic]=np.array(vector)
+
+    def genTest(self,k,j):
         threadTopic = {}
         for topic in self.Test.test_topics[j]:
             # self.vectorGen(topic,k,j)
-            threadTopic[topic] =  threading.Thread(target = self.vectorGen, args = (topic,k,j,))
+            threadTopic[topic] =  threading.Thread(target = self.vectorGenTest, args = (topic,k,j,))
             threadTopic[topic].start()
 
         for topic in self.Test.test_topics[j]:
             threadTopic[topic].join()
             print "Loaded", topic
 
-    def vectorGen(self,topic,k,j):
+    def vectorGenTest(self,topic,k,j):
         vector = [0 for x in range(k)]
         for word in self.Test.corpus[j][topic]:
             vector[self.Model.predict([self.Test.word_vectors[j][word]])[0]]+=1
