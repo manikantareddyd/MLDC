6943188fb540c109ebc0e214132d949c9e934962
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-04-03 13:17:21 +0530

    Pickle 1 and Split to main. Bug not fixed yet

--- src/freqModel/doc2vec.py
+++ src/freqModel/doc2vec.py
@@ -15,12 +15,14 @@ class docVector:
         self.topicVectors={}
         self.topicVectorsTrain={}
         threads = {}
+
         for j in self.languages:
             threads[j+'Train']=threading.Thread(target=self.genTrain, args=(k,j,))
             threads[j+'Train'].start()
         for j in self.languages:
             threads[j+'Train'].join()
         print "\nTraining Set Done\n"
+
         if test_list != None :
             for j in self.languages:
                 threads[j+'Test']=threading.Thread( target=self.genTest, args=(k,j,))
@@ -64,42 +66,3 @@ class docVector:
             vector[self.Model.predict([self.Test.word_vectors[j][word]])[0]]+=1
         vector = [math.exp((1+vector[x])/(1.0+1.0*max(vector))) for x in range(k)]
         self.topicVectors[topic]=np.array(vector)
-
-
-languages = ['en','fr','es']
-topics_list = ['Adele','Baboon','Chemistry','Energy','pokemon','English_language','French_language','India','Pakistan','Politics','Tennis','The_Beatles','Wikipedia']
-loo = len(topics_list)
-topics_list = topics_list[:loo]
-wordVecGen = wordVecGen(topics_list,languages,loo)
-test_list = ['pokemon','Energy','Tennis']
-Test = Test(wordVecGen, test_list)
-d=docVector(wordVecGen,Test,test_list,40)
-n = np.linalg.norm
-v=d.topicVectors
-neigh = NearestNeighbors(5,p=1)
-X_train=[]
-y_train=[]
-for x in d.topicVectorsTrain:
-    X_train.append(d.topicVectorsTrain[x])
-    y_train.append(x)
-neigh.fit(X_train,y_train)
-X = []
-y = []
-test_docs = []
-for l in languages:
-    for doc in test_list:
-        test_docs.append(doc+'_'+l)
-
-for topic in test_docs:
-    X.append(d.topicVectors[topic])
-    y.append(topic)
-
-dist, ind = neigh.kneighbors(X,n_neighbors=5,return_distance=True)
-table = PrettyTable(['Topic','N1','N2'])
-for i in range(len(X)):
-    table.add_row([y[int(i)],y_train[ind[i][1]],y_train[ind[i][2]]])
-
-print table
-
-# for topic in d.Test.test_topics['en']:
-#         table.add_row([topic,t,cosine(v[t],v[topic]),n(v[t]-v[topic],1),n(v[t]-v[topic],2)])
---
+++ src/freqModel/main.py
@@ -0,0 +1,51 @@
+from doc2vec import *
+# Declarations
+languages = ['en','fr','es']
+topics_list = ['Adele','Baboon','Chemistry','Energy','pokemon','English_language','French_language','India','Pakistan','Politics','Tennis','The_Beatles','Wikipedia']
+test_list = ['pokemon','Energy','Tennis']
+loo = len(topics_list)
+topics_list = topics_list[:loo]
+
+# All the generation
+# wordVecGen = wordVecGen(topics_list,languages,loo)
+# This is load wordVecGen directly. We don't compute it again and again
+import pickle
+f=open('word2vec.pkl','rb')
+wordVecGen = pickle.load(f)
+f.close()
+
+Test = Test(wordVecGen, test_list)
+d=docVector(wordVecGen,Test,test_list,40)
+
+#Empty lists
+X_train=[]
+y_train=[]
+
+for x in d.topicVectorsTrain:
+    X_train.append(d.topicVectorsTrain[x])
+    y_train.append(x)
+
+#NearestNeighbors finder
+neigh = NearestNeighbors(5,p=1)
+neigh.fit(X_train,y_train)
+
+test_docs = []
+for l in languages:
+    for doc in test_list:
+        test_docs.append(doc+'_'+l)
+
+# Test Set
+X = []
+y = []
+for topic in test_docs:
+    X.append(d.topicVectors[topic])
+    y.append(topic)
+
+#gives distance and indices in X_train, y_train of 5 NearestNeighbors of every element in X
+dist, ind = neigh.kneighbors(X,n_neighbors=5,return_distance=True)
+
+table = PrettyTable(['Topic','N1','N2'])
+for i in range(len(X)):
+    table.add_row([y[int(i)],y_train[ind[i][1]],y_train[ind[i][2]]])
+
+print table
